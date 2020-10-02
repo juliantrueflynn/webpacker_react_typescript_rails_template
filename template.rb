414314654e900555ef3effc5f349f5b6f5143a3a
Julian Flynn <hello@juliantrueflynn.com>
2020-10-02 19:33:49 -0400

    Init template for rubocop

---
+++ template.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+inject_into_file "Gemfile", after: "group :development do" do
+  <<-RUBY
+
+  gem "rubocop", "~> 0.83.0", require: false
+  gem "rubocop-rails", require: false
+  RUBY
+end
+
+# rubocop:disable Layout/LineLength
+run 'curl -L https://raw.githubusercontent.com/juliantrueflynn/webpacker_react_typescript_rails_template/main/.rubocop.yml > .rubocop.yml'
+# rubocop:enable Layout/LineLength
+
+after_bundle do
+  run 'rubocop -a'
+end
