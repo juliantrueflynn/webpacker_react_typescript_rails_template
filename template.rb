c5d59822930ce1bb99f8d1a6562402ea4bd7db83
Julian Flynn <hello@juliantrueflynn.com>
2020-10-02 20:31:45 -0400

    Setup default gem insertion

--- template.rb
+++ template.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# Cleanup Gemfile
+gsub_file "Gemfile", "gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]\n", ""
+gsub_file "Gemfile", "# Windows does not include zoneinfo files, so bundle the tzinfo-data gem\n", ""
+
+# Gems group: development, test
+inject_into_file "Gemfile", after: "group :development, :test do" do
+  <<-RUBY
+
+  gem "factory_bot_rails"
+  gem "rspec-rails"
+  RUBY
+end
+
+# Gems group: development
 inject_into_file "Gemfile", after: "group :development do" do
   <<-RUBY
 
@@ -8,10 +22,29 @@ inject_into_file "Gemfile", after: "group :development do" do
   RUBY
 end
 
-# rubocop:disable Layout/LineLength
-run "curl -L https://raw.githubusercontent.com/juliantrueflynn/webpacker_react_typescript_rails_template/main/.rubocop.yml > .rubocop.yml"
-# rubocop:enable Layout/LineLength
+# Gems group: test
+if File.read("Gemfile").match?(/group :test do/)
+  append_to_file "Gemfile", after: "group :test do" do
+    <<-RUBY
+
+    gem "faker"
+    gem "shoulda-matchers"
+    gem "simplecov", require: false
+    gem "webmock", require: false
+    RUBY
+  end
+else
+  gem_group :test do
+    gem "faker"
+    gem "shoulda-matchers"
+    gem "simplecov", require: false
+    gem "webmock", require: false
+  end
+end
 
 after_bundle do
+  # rubocop:disable Layout/LineLength
+  run "curl -L https://raw.githubusercontent.com/juliantrueflynn/webpacker_react_typescript_rails_template/main/.rubocop.yml > .rubocop.yml"
+  # rubocop:enable Layout/LineLength
   run "rubocop -a"
 end
